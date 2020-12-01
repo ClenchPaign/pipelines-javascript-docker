bbfa70435d98d042c22437135f9f181b3ce1d63a
Clench Paign <clep@softwareag.com>
2020-12-01 19:18:55 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world again4\n');
+  res.send('Hello world again5\n');
 });
 
 app.listen(PORT, HOST);
